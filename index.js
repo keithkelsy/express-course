051e6dd1c35bedfd8e8b89c4cd244e111204b6b1
Keith <55594418+keithkelsy@users.noreply.github.com>
2024-09-10 07:36:52 -0500

    plus: Queries

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/products', (req, res) => { 
-    res.send('Lista de productos')
+app.get('/search', (req, res) => {
+  if (req.query.q === 'javascript books') {
+    res.send('Libros de Javascript')
+  } else {
+    res.send('Pagina normal')
+  }
+
 })
 
-app.post('/products', (req, res) => { 
-    res.send('Creando lista de productos')
-})
-
-app.put('/products', (req, res) => { 
-    res.send('Actualizando productos')
-})
-
-app.delete('/products', (req, res) => { 
-    res.send('Eliminando lista de productos')
-})
-
-app.patch('/products', (req, res) => { 
-    res.send('Actualizando una parte del producto')
-})
 
 app.listen(3000);
 console.log(`Server running on ${3000}`);
-
