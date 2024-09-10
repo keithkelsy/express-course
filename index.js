95e013df5d9c1b0940411fa2fc89bd894b6895c4
Keith Kelsey <kkelsy@unal.edu.co>
2024-09-10 13:32:34 -0500

    plus: Middlewares created

--- index.js
+++ index.js
@@ -2,12 +2,46 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  console.log('Pasó por aquí');
-})
+
+
+app.use((req, res, next) => {
+  console.log(`Route: ${req.url} Method: ${req.method}`);
+  next()
+}) 
+
+
+const logger = () => {
+  
+}
+const isAuthenticated = () => {
+  
+}
+
 
 app.get('/profile', (req, res) => {
-  res.send('Profile page')
+  res.send('Profile Page')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/', (req, res) => {
+  res.send('Main page')
+})
+
+app.use((req, res, next) => {
+  if (req.query.login === 'kkelsy@unal.edu.co') {
+    next()
+  }
+  else {
+    res.send('403 - No estás autorizado')
+  }
+  next()
+})
+
+app.get('/dashboard', (req, res) => {
+  res.send('Dashboard page  ')
 })
 
 app.listen(3000);
